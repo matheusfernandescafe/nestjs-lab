9b94dbbee4f55dfb8710f6b0cad67e2b5091170a
Matheus Fernandes <matheusfernandesofc@gmail.com>
2026-09-10 22:50:54 -0300

    feat: criação de testes para criação de conta

--- src/tests/account.controller.e2e-spec.ts
+++ src/tests/account.controller.e2e-spec.ts
@@ -1,3 +1,39 @@
-test("1 plus 1", () => {
-  expect(1 + 1).toBe(2);
-});
+import { AppModule } from "@/app.module.js"
+import { PrismaService } from "@/prisma/prisma.service.js"
+import { INestApplication } from "@nestjs/common"
+import { Test } from "@nestjs/testing"
+import request from "supertest"
+
+describe("Create account (E2E)", () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts/create', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts/create').send({
+      name: 'User Test',
+      email: 'email@example.com',
+      password: '123',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'email@example.com'
+      }
+    })
+
+    expect((userOnDatabase)).toBeTruthy()
+  })
+})
